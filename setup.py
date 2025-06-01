a965afb3eb033ff7f4a85d7c5782b048f34e937a
NeoDev <neoforevershog@gmail.com>
2025-06-01 17:35:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-import os
-import platform
-import pkg_resources
 from setuptools import find_packages, setup
 
 setup(
     name="infer_rvc_python",
     version="1.1.0",
     description="Python wrapper for fast inference with rvc",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    readme="README.md",
     python_requires=">=3.10",
     author="R3gm",
     url="https://github.com/R3gm/infer_rvc_python",
     license="MIT",
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rep', '*.pickle']},
+    include_package_data=True,
     install_requires=[
         "torch",
         "torchaudio",
@@ -25,16 +22,17 @@ setup(
         "faiss-cpu==1.7.3",
         "torchcrepe==0.0.20",
         "ffmpeg-python>=0.2.0",
-        "fairseq==0.12.2",
+        "fairseq @ git+https://github.com/One-sixth/fairseq.git",
         "typeguard==4.2.0",
         "soundfile",
         "librosa",
         "numpy",
     ],
-    include_package_data=True,
-    extras_require={"all": [
-        "scipy",
-        "numba==0.56.4",
-        "edge-tts"
-        ]},
+    extras_require={
+        "all": [
+            "scipy",
+            "numba==0.56.4",
+            "edge-tts"
+        ]
+    },
 )
